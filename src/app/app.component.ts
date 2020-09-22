8ce95dfefc7f7f940786ffe18106370aef2855f2
Gioara Roberta Cuccu <gioara.roberta.cuccu@avanade.com>
2020-09-22 12:04:33 +0200

    add component welcome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alphashop';
+  //title = 'Alphashop';
+
+  saluti = 'Benvenuti nel sito ';
+  titolo2 = 'Seleziona gli articoli da acquistare:'
 }
